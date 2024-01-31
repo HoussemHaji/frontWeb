401a4146fd7aed8d8ae369d48559ffc4e94cb97f
Houssem Haji <houssemhaj87@gmail.com>
2024-01-31 14:58:02 +0100

    navbar update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,40 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./src/**/*.{html,ts}",
-    ],
-    darkMode: 'class',
-    theme: {
-        extend: {
-            colors: {
+import withMT from "@material-tailwind/html/utils/withMT";
 
-                primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" }
-            },
-            height: {
-                '80': '20rem',
-                '82': '20.5rem',
-                '84': '21rem',
-                '90': '22.5rem',
-                '128': '32rem',
-                '144': '36rem',
-                '160': '40rem',
-                '176': '44rem',
-                '192': '48rem',
-            },
-            screens: {
-                'xs': '400px',
-                '3xl': '1680px',
-                '4xl': '2200px',
-            },
-            maxWidth: {
-                '10xl': '1512px',
-            },
-            borderRadius: {
-                '5xl': '40px',
-            },
-        },
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+
+        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" }
+      },
+      height: {
+        '80': '20rem',
+        '82': '20.5rem',
+        '84': '21rem',
+        '90': '22.5rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '176': '44rem',
+        '192': '48rem',
+      },
+      screens: {
+        'xs': '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      },
+      borderRadius: {
+        '5xl': '40px',
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+})
