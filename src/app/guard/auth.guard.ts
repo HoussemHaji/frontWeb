2496ef272d9237332e59e599d0cefee3a8b9b512
Houssem Haji <houssemhaj87@gmail.com>
2024-02-04 22:48:58 +0100

    add profile section in home v2

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,27 +1,33 @@
 // auth.guard.ts
 
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-
-
-    if((typeof window !== 'undefined') && (localStorage.getItem('token'))) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }}
-
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('token')) {
+        return true;
+      } else {
+        this.router.navigate(['/login']);
+        return false;
+      }
+    }
+    return false;
+  }
 }
